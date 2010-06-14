862f45542800f202567002f523c1c4b1b206060e
diegompin@gmail.com <diegompin@gmail.com@fbd7b21e-e854-1ece-dda1-815532225474>
2010-06-14 00:24:47 +0000

    Altera;áo para o build rodar

--- trunk/Implementação/Fontes/War/src/br/upe/war/testes/TesteTodos.java
+++ trunk/Implementação/Fontes/War/src/br/upe/war/testes/TesteTodos.java
@@ -2,7 +2,6 @@ package br.upe.war.testes;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import br.upe.war.testes.testeUC02.TesteUC02;
 import br.upe.war.testes.unitarios.TesteUnitariosTodos;
 
 public class TesteTodos 
@@ -11,7 +10,6 @@ public class TesteTodos
 	{
 		TestSuite suite = new TestSuite("Todos casos de uso e testes unitários");
 		
-		suite.addTest(TesteUC02.suite());
 		suite.addTest(TesteUnitariosTodos.suite());
 		
 		return suite;
